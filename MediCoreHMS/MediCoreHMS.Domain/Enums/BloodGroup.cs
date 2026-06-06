6867802fff812354306ac6700b5e3e269364d5c6
alasaneeth <alasaneeth22@gmail.com>
2026-06-06 14:06:00 +0530

    feat(domain): add Gender, AppointmentStatus and BloodGroup enums

---
+++ MediCoreHMS/MediCoreHMS.Domain/Enums/BloodGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCoreHMS.Domain.Enums
+{
+    public enum BloodGroup
+    {
+        APositive,
+        ANegative,
+        BPositive,
+        BNegative,
+        OPositive,
+        ONegative,
+        ABPositive,
+        ABNegative
+    }
+}
---
+++ MediCoreHMS/MediCoreHMS.Domain/Enums/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace MediCoreHMS.Domain.Enums;
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other
+}
