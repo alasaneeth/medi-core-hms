67fa72e84e5852d87f86dd432774e98ab1156a03
alasaneeth <alasaneeth22@gmail.com>
2026-06-07 13:55:19 +0530

    feat(infrastructure): configure AppDbContext dependency injection and connection string

--- MediCoreHMS/MediCoreHMS.Web/Program.cs
+++ MediCoreHMS/MediCoreHMS.Web/Program.cs
@@ -1,7 +1,16 @@
+using MediCoreHMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+// Database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
