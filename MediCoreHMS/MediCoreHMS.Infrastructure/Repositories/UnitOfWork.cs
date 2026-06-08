7c34180f223cadbba3d64cbc5357d6d1d12721a4
alasaneeth <alasaneeth22@gmail.com>
2026-06-08 11:56:00 +0300

    feat(infrastructure): add UnitOfWork implementation

---
+++ MediCoreHMS/MediCoreHMS.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using MediCoreHMS.Application.Interfaces;
+using MediCoreHMS.Infrastructure.Data;
+using MediCoreHMS.Infrastructure.Repositories;
+
+namespace MediCoreHMS.Infrastructure.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public IUserRepository Users { get; private set; }
+    public IRoleRepository Roles { get; private set; }
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        Users = new UserRepository(context);
+        Roles = new RoleRepository(context);
+    }
+
+    public async Task<int> SaveChangesAsync()
+        => await _context.SaveChangesAsync();
+
+    public void Dispose()
+        => _context.Dispose();
+}
