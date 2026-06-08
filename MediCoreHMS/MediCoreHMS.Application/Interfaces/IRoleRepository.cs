392235175201b53e122b9b46c5c9c9f8e613b9fb
alasaneeth <alasaneeth22@gmail.com>
2026-06-08 13:41:35 +0530

    feat(application): add IGenericRepository, IUserRepository and IRoleRepository interfaces

---
+++ MediCoreHMS/MediCoreHMS.Application/Interfaces/IRoleRepository.cs
@@ -0,0 +1,8 @@
+﻿using MediCoreHMS.Domain.Entities;
+
+namespace MediCoreHMS.Application.Interfaces;
+
+public interface IRoleRepository : IGenericRepository<Role>
+{
+    Task<Role?> GetByNameAsync(string name);
+}
---
+++ MediCoreHMS/MediCoreHMS.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using MediCoreHMS.Domain.Entities;
+
+namespace MediCoreHMS.Application.Interfaces;
+
+public interface IUserRepository : IGenericRepository<User>
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task<bool> EmailExistsAsync(string email);
+}
