389f8a34457c15b209e88a186afd0746e253f867
alasaneeth <alasaneeth22@gmail.com>
2026-06-08 13:42:49 +0530

    feat(application): add IUnitOfWork interface with repository properties

---
+++ MediCoreHMS/MediCoreHMS.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace MediCoreHMS.Application.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository Users { get; }
+    IRoleRepository Roles { get; }
+    Task<int> SaveChangesAsync();
+}
