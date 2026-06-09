b2698637675f43479e76c4c783c6563c6713b4ca
alasaneeth <alasaneeth22@gmail.com>
2026-06-09 11:36:02 +0300

    feat(web): add DashboardController and Dashboard Index view with role display

---
+++ MediCoreHMS/MediCoreHMS.Web/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediCoreHMS.Web.Controllers;
+
+[Authorize]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        var role = User.FindFirst(System.Security.Claims.ClaimTypes.Role)?.Value;
+        var name = User.FindFirst(System.Security.Claims.ClaimTypes.Name)?.Value;
+
+        ViewBag.Role = role;
+        ViewBag.Name = name;
+
+        return View();
+    }
+}
